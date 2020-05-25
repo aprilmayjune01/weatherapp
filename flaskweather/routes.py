9fef820c6f838bf90708fe0958375878fbe8698d
Lewis293 <32829395+Lewis293@users.noreply.github.com>
2020-05-25 19:30:27 +0100

    Add files via upload

--- flaskweather/routes.py
+++ flaskweather/routes.py
@@ -6,8 +6,19 @@ from flaskweather import app, db, bcrypt
 from flaskweather.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskweather.models import User
 from flask_login import login_user, current_user, logout_user, login_required
+from bs4 import BeautifulSoup
+import requests
 
-@app.route("/")
+global locationCityName
+locationCityName = "Cardiff"
+
+global tempRaw
+global time
+global location
+global phrase
+global chosenStyle 
+chosenStyle = "Both"
+
 @app.route("/home")
 def home():
 	return render_template('home.html')
@@ -81,3 +92,137 @@ def account():
     image_file = url_for('static', filename='profile_picture/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
 
+
+def template(text = ""):
+    templateDate = {
+        'text' : text,
+        'tvalues' : (" ","Cardiff", "York", "Portsmouth","Swansea","Manchester","London","Leeds","Sheffield",
+            "Bradford","Liverpool","Bristol","Sunderland","Edinburgh","Exeter","Dublin"),
+        'selected_tvalue' : "unselected. Please select a city."
+    }
+    return templateDate
+
+
+@app.route("/location", methods=['POST', 'GET'])
+def location():
+    tvalue= "unselected. Please select a city."
+    if request.method == "POST":            
+        tvalue = str(request.form['tvalue'])
+
+    global locationCityName
+    locationCityName = tvalue
+
+    templateData = template(text = tvalue)
+    templateData['selected_tvalue'] = tvalue 
+
+    if locationCityName == ' ':
+        locationCityName = "Cardiff"
+        templateData = template(text = locationCityName)
+        templateData['selected_tvalue'] = locationCityName
+    return render_template('dropdown.html',**templateData)
+
+    if tvalue == 'unselected. Please select a city.':
+        locationCityName = "Cardiff"
+        templateData = template(text = locationCityName)
+        templateData['selected_tvalue'] = locationCityName
+
+    return render_template('dropdown.html',**templateData)
+
+    return render_template('dropdown.html', **templateData)
+
+@app.route('/result/')
+def result():
+
+    locations = {
+            'Cardiff': {'lati':51.48,'longi':-3.18},
+            'York': {'lati':54.00,'longi':-1.08},
+            'Portsmouth': {'lati':50.82,'longi':-1.09},
+            'Swansea': {'lati':51.62,'longi':-3.94},
+            'Manchester': {'lati':53.48,'longi':-2.24},
+            'London': {'lati':51.48,'longi':0},
+            'Leeds':{'lati':53.80,'longi':-1.55},
+            'Sheffield':{'lati':53.40,'longi':-1.47},
+            'Bradford':{'lati':53.80,'longi':-1.76},
+            'Liverpool':{'lati':53.40,'longi':-2.98},
+            'Bristol':{'lati':51.45,'longi':-2.59},
+            'Sunderland':{'lati':54.90,'longi':-1.38},
+            'Edinburgh':{'lati':55.95,'longi':-3.19},
+            'Exeter':{'lati':50.72,'longi':-3.53 },
+            'Dublin':{'lati':53.34,'longi':-6.26},                  
+            }
+
+    global locationCityName
+    searchCity = str(locationCityName)
+
+    try:
+        lat = str(locations[searchCity].get('lati'))
+    except KeyError:
+        lat = "51.48"
+
+    try:
+        lon = str(locations[searchCity].get('longi'))
+    except KeyError:
+        lon = "-3.18"
+
+    weatherUrl=('https://weather.com/en-GB/weather/today/l/'+lat+','+lon+'?par=google&temp=c')
+
+    source = requests.get(weatherUrl).text
+
+    soup = BeautifulSoup(source, 'lxml')
+
+    global tempRaw
+    global time
+    global location
+    global phrase
+
+    tempRaw = soup.find("div",{"class" : "today_nowcard-temp"}).text
+    time = soup.find('body').select_one('p').text
+    location = soup.find('body').select_one('h1').text
+    phrase = soup.find("div",{"class" : "today_nowcard-phrase"}).text
+
+    return render_template('weatherLocation.html',tempOut = tempRaw, cityOut = "in " + location + " " + time, phraseOut = phrase)
+
+
+@app.route('/settings/', methods= ['POST','GET'])
+def settings():
+
+    if request.method == 'POST':
+        global chosenStyle
+        chosenStyle = request.form["options"]
+
+    return render_template("settings.html", chosenStyleOut = chosenStyle)
+
+
+@app.route('/outfit/')
+def outfit():
+
+    global tempRaw
+    global time
+    global location
+    global phrase
+    global chosenStyle
+
+    outfits = {
+        'Sunny': {'male':'T-shirt and Shorts, Sunglasses, Trainers','female':'Summer Dress, Sunglasses, Sandals'},
+        'Cloudy': {'male':'T-shirt and Jeans, Denim Jacket, Sunglasses, Trainers','female':'T-shirt and Skirt, Denim Jacket, Sunglasses, Trainers'},
+        'Rainy': {'male':'T-shirt and Jeans, Waterproof shoes, Raincoat, Umbrella','female':'Cardigan and Jeans, Waterproof shoes, Raincoat, Umbrella'},
+        'Fog': {'male':'Cardigan and Jeans, Shoes, Raincoat','female':'Cardigan and Jeans, Shoes, Raincoat'},
+        'Thunderstorms': {'male':'T-shirt and Jeans, Waterproof shoes, Raincoat, Umbrella','female':'Cardigan and Jeans, Waterproof shoes, Raincoat, Umbrella'}, 
+        'Partly Cloudy': {'male':'long-sleeve T-shirt and Jeans, Sweater, Scarf','female':'long-sleeve T-shirt and Jeans, Sweater, Scarf'},
+        'Fair': {'male':'T-shirt, Jeans, Sunglasses, Trainers','female':'Summer Dress, Sunglasses, Trainers'},                 
+    }
+
+    maleOutfit = ""
+    femaleOutfit = ""
+
+    maleOutfit = str(outfits[phrase].get('male'))
+    femaleOutfit = str(outfits[phrase].get('female'))
+
+    if chosenStyle == "Male":
+        allOutfit = maleOutfit
+    elif chosenStyle == "Female":
+        allOutfit = femaleOutfit
+    elif chosenStyle == "Both":
+        allOutfit = maleOutfit + " | " + femaleOutfit
+
+    return render_template("display.html", phraseOut = phrase, outfitOut = allOutfit, styleOut = chosenStyle, locationOut = location)
